92a5d904328cc4f4a56a34879f3069bfd523b8b0
tagghadshi <tagghadshi@gmail.com>
2021-09-02 12:10:38 +0000

    microservice practise

---
+++ ribbon-eureka-server/src/test/java/com/example/ribboneurekaserver/RibbonEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ribboneurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
